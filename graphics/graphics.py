7577fd7b67b30c1fb8b6d19e2857bfd1a1868dca
csa-slo <cspreen@calpoly.edu>
2023-01-11 00:48:03 -0800

    updated graphics.py

--- graphics/graphics.py
+++ graphics/graphics.py
@@ -2,31 +2,28 @@
 from os import get_terminal_size
 from time import sleep
 
-# Initialize constants
-position = int(input("Enter starting position: "))
-image = input("Enter an image: ")
+# Draw one line graphics with given string to animate, starting position, trajectory
+def one_line_graphics(image, position, trajectory):
 
-# Draws an  
-def one_line_graphics(position, trajectory, image):
-
-    #Get terminal size
+    # Get terminal size
     term_size = get_terminal_size()
 
-    #Draw loop
+    # Draw loop
     while True:
 
-        #Draw the current frame
+        # Draw a frame
         print(" " * position + image)
 
-        #Limit framerate to 60FPS
-        sleep(1/60)
-
-        #Clear the terminal
-        print("\n" * term_size.lines)
-
-        #Calculate next position
+        # Calculate position for next frame
         position = trajectory(position, image, term_size)
 
+        # Limit framerate to 60FPS
+        sleep(1/60)
+
+        # Clear the terminal
+        print("\n" * term_size.lines)
+
+# Specify a constant speed of motion
 def constant_velocity(position, image, term_size):
         if (position + len(image)) % term_size.columns == 0:
             position = 0
@@ -34,4 +31,7 @@ def constant_velocity(position, image, term_size):
             position += 1
         return position
 
-one_line_graphics(position, constant_velocity, image)
+# Main
+initial_position = int(input("Enter starting position: "))
+initial_image = input("Enter an image: ")
+one_line_graphics(initial_image, initial_position, constant_velocity)
